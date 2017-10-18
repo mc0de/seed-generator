bbd4e50df449679cfb0726273ef0069b4d15c0fd
David Lun <mysticcode@gmail.com>
2017-10-19 01:30:37 +0300

    seeding performance and bugfixes

--- seedgen.py
+++ seedgen.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
-import re, sys, os, getopt
+import re
+import sys
+import os
+import getopt
 
 
 def generate_seeds(db_values, table_name, fields):
-    opath = 'output/database/migrations/seeds/'
+    opath = 'output/'
     fname = camel_case(table_name) + 'Seed.php'
     print("Generating: {}".format(fname))
     tabsize = 4
@@ -12,19 +15,21 @@ def generate_seeds(db_values, table_name, fields):
         os.makedirs(opath)
     f = open(opath + fname, 'w')
     f.write("<?php\n\n")
-    f.write("use Illuminate\Database\Seeder;\n")
-    f.write("use DB;\n\n")
-    f.write("class " + camel_case(table_name) + "Seed extends Seeder\n{\n") # class open
-    f.write(' ' * tabsize + "/**\n" + ' ' * tabsize + " * Run the database seeds.\n")
-    f.write(' ' * tabsize + " *\n" + ' ' * tabsize + " * @return void\n" + ' ' * tabsize + " */\n")
+    f.write("use Illuminate\Database\Seeder;\n\n")
+    f.write("class " + camel_case(table_name) +
+            "Seed extends Seeder\n{\n")  # class open
+    f.write(' ' * tabsize + "/**\n" + ' ' *
+            tabsize + " * Run the database seeds.\n")
+    f.write(' ' * tabsize + " *\n" + ' ' * tabsize +
+            " * @return void\n" + ' ' * tabsize + " */\n")
     f.write(' ' * tabsize + "public function run()\n" + ' ' * tabsize + "{\n")
     f.write(' ' * tabsize * 2 + "$items = [\n")
     for i, value in enumerate(db_values):
-        f.write(' ' * tabsize * 3 +'[')
+        f.write(' ' * tabsize * 3 + '[')
         for j, field in enumerate(value):
             if type(value[j]) is int:
                 f.write("'{}' => {}".format(fields[j], value[j]))
-            elif value[j] is None :
+            elif value[j] is None:
                 f.write("'{}' => NULL".format(fields[j], value[j]))
             else:
                 f.write("'{}' => '{}'".format(fields[j], value[j]))
@@ -35,18 +40,10 @@ def generate_seeds(db_values, table_name, fields):
         else:
             f.write('],\n')
     f.write(' ' * tabsize * 2 + "];\n\n")
-    f.write(' ' * tabsize * 2 + "foreach ($items as $item) {\n")
-    f.write(' ' * tabsize * 3 + "DB::insert('insert into " + table_name +" (")
-    [f.write(field + ")") if i == len(fields) - 1 else f.write(field + ', ') for i, field in enumerate(fields)]
-    f.write(' values (')
-    [f.write("?)'") if i == len(fields) - 1 else f.write('?, ') for i, field in enumerate(fields)]
-    f.write(", [\n")
-    [f.write(' ' * tabsize * 4 + "$item['" + field + "']\n") if i == len(fields) - 1 else f.write(' ' * tabsize * 4 + "$item['" + field + "'],\n") for i, field in enumerate(fields)]
-    f.write(' ' * tabsize * 3 + "]")
-    f.write(');\n') # closes db insert
-    f.write(' ' * tabsize * 2 + "}\n")
-    f.write(' ' * tabsize + "}\n") # run end
-    f.write("}\n") # class end
+    f.write(' ' * tabsize * 2 + "DB::table('" +
+            table_name + "')->insert($items);\n")
+    f.write(' ' * tabsize + "}\n")  # run end
+    f.write("}\n")  # class end
     f.close()
 
 
@@ -86,7 +83,7 @@ def camel_case(s):
 
 
 def usage():
-    return """Usage: python"""+sys.argv[0]+""" [OPTION]...
+    return """Usage: python""" + sys.argv[0] + """ [OPTION]...
 Tool description
 
   -h, --help
@@ -94,6 +91,7 @@ Tool description
   -i, --input=FILE
         SQL file to make seeds of
 """
+
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
